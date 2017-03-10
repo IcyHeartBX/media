08cd237eccb835c4db2408b64c87868de8e1358b
pixboly <tpxsky@163.com>
2017-03-10 10:07:46 +0800

    add all

---
+++ code/TsRtpServerDemo/TestMain.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : TsRtpServerDemo.cpp
+// Author      : pix
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "test_ts_rtp_server.h"
+
+
+int main(void) {
+	puts("Hello World!!!");
+	test_ts_rtp_server();
+	return EXIT_SUCCESS;
+}
---
+++ code/TsRtpServerDemo/test_ts_rtp_server.cpp
@@ -0,0 +1,16 @@
+/*
+ * test_ts_rtp_server.cpp
+ *
+ *  Created on: 2017年3月10日
+ *      Author: pixboly
+ */
+
+#include "test_ts_rtp_server.h"
+#include <iostream>
+using namespace std;
+
+void test_ts_rtp_server(void)
+{
+	const char * TAG = "test_ts_rtp_server()";
+	cout<<TAG<<",RUN..."<<endl;
+}
